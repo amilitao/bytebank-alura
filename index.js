ae33e021f0bdf498ddd1ce9793a3af065b16c4ae
amilitao <adriano.militao@yahoo.com.br>
2021-01-10 11:26:50 -0300

    primeiro commit

---
+++ index.js
@@ -0,0 +1,14 @@
+import {Gerente} from "./Gerente.js"
+import {Diretor} from "./Diretor.js"
+import {SistemaAutenticacao} from "./SistemaAutenticacao.js"
+import { Cliente } from "./Cliente.js";
+
+
+const diretor = new Diretor("Rodrigo", 10000, 123456789);
+diretor.cadastrarSenha("123456");
+const gerente = new Gerente("Ricardo", 5000, 222333444);
+const cliente = new Cliente("Carlos", 123456777, "123");
+
+const estaLogado = SistemaAutenticacao.login(cliente, "133");
+
+console.log(estaLogado);
